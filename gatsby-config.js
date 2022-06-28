6f5061f22f72e04ba6a15b92773328c432a2dab6
Rasmus Christiansen <rbchristiansen@gmail.com>
2022-06-28 21:35:33 +0200

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,9 +8,9 @@ module.exports = {
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
     siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
+    siteTitleAlt: `Rasmus S. Christiansen`,
+    siteHeadline: `Analytics Thoughts`,
+    siteUrl: `https://rbchristiansen.com/`,
     siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
